efebc34e8acb9410cdfc92779f6a2dab455e7305
Karan Joshi <Karan190213@gmail.com>
2023-09-05 18:08:54 +0530

    Setup project: Initial commit
    In this commit, I've completed the setup for the Leaderboard JavaScript project, focusing on implementing webpack, ES6 features, and modular JavaScript.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Exercise',
+      template: './src/template.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
