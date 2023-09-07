0adf3c8a5de5483709879eb5eca2bc5802e190cc
Karan Joshi <Karan190213@gmail.com>
2023-09-07 16:37:18 +0530

    Implement API communication for Leaderboard app
    In this commit, I have implemented the functionality to send and receive data from the Leaderboard API for the Leaderboard list app.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './style/styles.css'; // import the stylesheet
-import './modules/FormSubmission.js'; // import the module that handles the form submission
+import { renderScores } from './modules/leaderboard.js'; // import the renderScores function
+import './modules/form.js'; // import the module that handles the form submission
+
+// Render the initial list when the page loads
+renderScores();
---
+++ src/modules/api.js
@@ -0,0 +1,36 @@
+const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RnKbpaBQTMy4ZFjEty89/scores/';
+
+// Function to fetch scores from the API
+const fetchScores = async () => {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    console.error('Error fetching scores:', error);
+    return [];
+  }
+};
+
+// Function to submit a new score to the API
+const submitScore = async (playerName, playerScore) => {
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: playerName,
+        score: playerScore,
+      }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error submitting score:', error);
+    return { result: 'error', message: 'Unable to submit score' };
+  }
+};
+
+export { fetchScores, submitScore };
